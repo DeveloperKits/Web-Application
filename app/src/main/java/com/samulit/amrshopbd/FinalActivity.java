70cc045b946367727f1f4dbf9b5be2e4a20154ba
DeveloperKits <69858580+DeveloperKits@users.noreply.github.com>
2021-07-08 20:42:16 +0600

    update

--- app/src/main/java/com/samulit/amrshopbd/FinalActivity.java
+++ app/src/main/java/com/samulit/amrshopbd/FinalActivity.java
@@ -3,8 +3,10 @@ package com.samulit.amrshopbd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,26 +22,44 @@ public class FinalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_final);
 
         myWebView = (WebView) findViewById(R.id.WebView);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://amrshopbd.com/");
 
         // For JavaScript Enable
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.getAllowContentAccess();
+
+        myWebView.loadUrl("https://speedbazarbd.com/");
+        myWebView.setWebViewClient(new WebViewClient());
     }
 
-    public static class myWebClient extends WebViewClient{
+    private class MyWebViewClient extends WebViewClient {
         @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon){
-            super.onPageStarted(view,url,favicon);
-        }
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view,String url){
-            view.loadUrl(url);
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            if ("https://speedbazarbd.com/".equals(request.getUrl().getHost())) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+            startActivity(intent);
             return true;
         }
     }
+
+    private class myWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith("https://speedbazarbd.com/")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }else {
+                view.loadUrl(url);
+                return true;
+            }
+        }
+    }
+
 
     @Override
     public void onBackPressed(){
